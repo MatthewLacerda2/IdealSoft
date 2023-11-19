ebceca8c44956bcf2dca9caa840f8e4dd8909575
Matheus Lacerda <matheus.l1996@gmail.com>
2023-11-19 19:05:05 -0300

    post method

--- BackEnd/Data/BackendDbContext.cs
+++ BackEnd/Data/BackendDbContext.cs
@@ -1,13 +1,13 @@
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using System;
 
 
 namespace BackEnd.Data;
 public class BackendDbContext : IdentityDbContext {
+
     public BackendDbContext(DbContextOptions<BackendDbContext> options) : base(options) {
     }
 
     public DbSet<User> Users { get; set; }
+
 }
--- BackEnd/Models/UserDTO.cs
+++ BackEnd/Models/UserDTO.cs
@@ -1,9 +1,28 @@
-using System;
-
 namespace Backend.Models;
 public class UserDto {
     public string Id { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
-    public string Name { get; set; } = string.Empty;
+    public string UserName { get; set; } = string.Empty;
     public DateTime CreationDate { get; set; } = DateTime.UtcNow;
+
+    public static explicit operator User(UserDto dto){
+        return new User {
+            Id = dto.Id,
+            UserName = dto.UserName,
+            Email = dto.Email,
+            CreationDate = dto.CreationDate,
+
+            NormalizedUserName = dto.UserName.ToUpper(),
+            NormalizedEmail = dto.Email.ToUpper()
+        };
+    }
+
+    public static explicit operator UserDto(User _user){
+        return new UserDto {
+            Id = _user.Id,
+            UserName = _user.UserName!,
+            Email = _user.Email!,
+            CreationDate = _user.CreationDate
+        };
+    }
 }
