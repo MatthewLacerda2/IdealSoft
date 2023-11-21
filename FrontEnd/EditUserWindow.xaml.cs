2e6da28f5d8c46289cfb5b5c3d0632a7f1f28028
Matheus Lacerda <matheus.l1996@gmail.com>
2023-11-21 12:31:33 -0300

    all wpf windows finished

--- FrontEnd/EditUserWindow.xaml.cs
+++ FrontEnd/EditUserWindow.xaml.cs
@@ -1,16 +1,19 @@
-﻿// EditUserWindow.xaml.cs
-using BackEnd.Models;
+﻿using BackEnd.Models;
 using System.Windows;
 
 namespace FrontEnd;
 
 public partial class EditUserWindow : Window {
+
+    private readonly ApiService apiService;
+
     public User EditedUser { get; private set; }
 
     public EditUserWindow(User user) {
         InitializeComponent();
         DataContext = this;
 
+        apiService = new ApiService();
         EditedUser = user;
 
         NameTextBox.Text = user.name;
@@ -18,9 +21,23 @@ public partial class EditUserWindow : Window {
         TelephoneTextBox.Text = user.telephone;
     }
 
-    private void SaveButton_Click(object sender, RoutedEventArgs e) {
-        MessageBox.Show("Editing saved!", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+    private async void SaveButton_Click(object sender, RoutedEventArgs e) {
+        // Update the EditedUser with the values from the TextBoxes
+        EditedUser.name = NameTextBox.Text;
+        EditedUser.surname = SurnameTextBox.Text;
+        EditedUser.telephone = TelephoneTextBox.Text;
+
+        // Make an HTTP PATCH call to update the user
+        bool updateResult = await apiService.UpdateUser(EditedUser);
+
+        if (updateResult) {
+            MessageBox.Show("Editing saved!", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+            Close();
+        } else {
+            MessageBox.Show("Failed to save changes.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
     }
+
 
     private void DoneButton_Click(object sender, RoutedEventArgs e) {
         Close();
