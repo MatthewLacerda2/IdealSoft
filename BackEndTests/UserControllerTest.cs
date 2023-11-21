6fceb94e5835965d4f8028d31d8afafda0bb4ec0
Matheus Lacerda <matheus.l1996@gmail.com>
2023-11-21 14:43:49 -0300

    ControllerBase tested

--- BackEndTests/UserControllerTest.cs
+++ BackEndTests/UserControllerTest.cs
@@ -5,15 +5,6 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
 using Newtonsoft.Json;
-using System;
-using System.Threading.Tasks;
-using Xunit;
-
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Newtonsoft.Json;
 
 namespace BackEndTests {
 
@@ -71,6 +62,118 @@ namespace BackEndTests {
             var deserializedUser = JsonConvert.DeserializeObject<User>(userJson);
 
             Assert.Equal(user.Id, deserializedUser.Id);
+            Assert.Equal(user.name, deserializedUser.name);
+            Assert.Equal(user.surname, deserializedUser.surname);
+            Assert.Equal(user.telephone, deserializedUser.telephone);
+        }
+
+        [Fact]
+        public async Task PostUser_ReturnsBadRequest_WhenIdAlreadyExists() {
+            // Arrange
+            int existingId = 74;
+
+            var controller = new UserController(_context);
+            var existingUser = new User { Id = existingId, name = "ExistingName", surname = "ExistingSurname", telephone = "987654321" };
+            await _context.Users.AddAsync(existingUser);
+            await _context.SaveChangesAsync();
+
+            var newUserWithExistingId = new User { Id = existingId, name = "NewName", surname = "NewSurname", telephone = "123456789" };
+
+            // Act
+            var result = await controller.PostUser(newUserWithExistingId);
+
+            // Assert
+            var badRequestResult = Assert.IsType<BadRequestObjectResult>(result.Result);
+            Assert.Equal("Id already taken!", badRequestResult.Value);
+        }
+
+        [Fact]
+        public async Task PostUser_ReturnsCreatedAtAction_WhenUserFound() {
+            // Arrange
+            var controller = new UserController(_context);
+            var newUser = new User { Id = 95, name = "TestName", surname = "TestSurname", telephone = "123456789" };
+
+            // Act
+            var result = await controller.PostUser(newUser);
+
+            // Assert
+            var createdAtActionResult = Assert.IsType<CreatedAtActionResult>(result.Result);
+            var createdUser = Assert.IsType<User>(createdAtActionResult.Value);
+            Assert.Equal(newUser.Id, createdUser.Id);
+            Assert.Equal(newUser.name, createdUser.name);
+            Assert.Equal(newUser.surname, createdUser.surname);
+            Assert.Equal(newUser.telephone, createdUser.telephone);
+        }
+
+        [Fact]
+        public async Task PatchUser_ReturnsOk_WhenIdExists() {
+            // Arrange
+            int existingId = 111;
+            var controller = new UserController(_context);
+            var existingUser = new User { Id = existingId, name = "ExistingName", surname = "ExistingSurname", telephone = "987654321" };
+            await _context.Users.AddAsync(existingUser);
+            await _context.SaveChangesAsync();
+
+            var updatedUserData = new User { Id = existingId, name = "UpdatedName", surname = "UpdatedSurname", telephone = "123456789" };
+
+            // Act
+            var result = await controller.PatchUser(updatedUserData);
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            var updatedUser = Assert.IsType<User>(okResult.Value);
+            Assert.Equal("UpdatedName", updatedUser.name);
+            Assert.Equal("UpdatedSurname", updatedUser.surname);
+            Assert.Equal("123456789", updatedUser.telephone);
+        }
+
+        [Fact]
+        public async Task PatchUser_ReturnsBadRequest_WhenIdNotFound() {
+            // Arrange
+            var controller = new UserController(_context);
+            var updatedUserData = new User { Id = 134, name = "UpdatedName", surname = "UpdatedSurname", telephone = "123456789" };
+
+            // Act
+            var result = await controller.PatchUser(updatedUserData);
+
+            // Assert
+            var badRequestResult = Assert.IsType<BadRequestObjectResult>(result);
+            Assert.Equal("User Id not found!", badRequestResult.Value);
+        }
+
+
+
+        [Fact]
+        public async Task DeleteUser_ReturnsNoContent_WhenIdExists() {
+            // Arrange
+            int existingId = 115;
+
+            var controller = new UserController(_context);
+            var existingUser = new User { Id = existingId, name = "ExistingName", surname = "ExistingSurname", telephone = "987654321" };
+            await _context.Users.AddAsync(existingUser);
+            await _context.SaveChangesAsync();
+
+            // Act
+            var result = await controller.DeleteUser(existingId);
+
+            // Assert
+            Assert.IsType<NoContentResult>(result);
+
+            var deletedUser = await _context.Users.FindAsync(existingId);
+            Assert.Null(deletedUser);
+        }
+
+        [Fact]
+        public async Task DeleteUser_ReturnsBadRequest_WhenIdNotFound() {
+            // Arrange
+            var controller = new UserController(_context);
+
+            // Act
+            var result = await controller.DeleteUser(172);
+
+            // Assert
+            var badRequestResult = Assert.IsType<BadRequestObjectResult>(result);
+            Assert.Equal("User Id not found!", badRequestResult.Value);
         }
     }
 }
